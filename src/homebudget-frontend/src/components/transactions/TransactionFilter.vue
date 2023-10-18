e0cb90328078af69b5858630e13b8fb3290ba410
MacGurk <oliver.morf@morphnet.ch>
2023-10-18 22:05:47 +0200

    make transaction filter mobile friendly

--- src/homebudget-frontend/src/components/transactions/TransactionFilter.vue
+++ src/homebudget-frontend/src/components/transactions/TransactionFilter.vue
@@ -1,108 +1,111 @@
 <template>
   <div class="d-flex justify-center align-center">
-    <v-icon size="large" @click="this.year--">
-      mdi-chevron-double-left
-    </v-icon>
+    <v-icon size="large" @click="year--"> mdi-chevron-double-left </v-icon>
     <div class="mx-3">
       <v-text-field
-          ref="yearText"
-          type="text"
-          v-model="enteredYear"
-          v-if="editYear"
-          @blur="finishEditYear"
-          @keydown:enter="finishEditYear"
+        v-if="editYear"
+        ref="yearText"
+        v-model="enteredYear"
+        type="text"
+        @blur="finishEditYear"
+        @keydown:enter="finishEditYear"
       ></v-text-field>
       <div v-else class="text-h6" @click="startEditYear">
         {{ year }}
       </div>
     </div>
-    <v-icon size="large" @click="this.year++">
-      mdi-chevron-double-right
-    </v-icon>
+    <v-icon size="large" @click="year++"> mdi-chevron-double-right </v-icon>
   </div>
   <div class="d-flex justify-space-evenly ma-3">
+    <v-select
+      v-if="mobile"
+      v-model="selectedMonth"
+      :items="allMonths"
+      item-value="index"
+      item-title="name"
+    ></v-select>
     <div
-        class="month-box border v-table__wrapper flex-column flex-grow-1 text-center py-2"
-        :class="monthActive(index)"
-        v-bind:key="index" v-for="(month, index) in allMonths"
-        @click="changeMonth(index)"
+      v-for="month in allMonths"
+      v-else
+      :key="month.index"
+      class="month-box border v-table__wrapper flex-column flex-grow-1 text-center py-2"
+      :class="monthActive(month.index)"
+      @click="changeMonth(month.index)"
     >
-      {{ month }}
+      {{ month.name }}
     </div>
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { monthNames } from '@/utils/utils';
-import {defineComponent} from 'vue';
+import { ref, watch, nextTick } from 'vue';
+import { useDisplay } from 'vuetify';
 
-interface TransactionFilterData{
-  month: number;
-  allMonths: String[];
-  year: number;
-  editYear: boolean
-  enteredYear: number;
-}
+const yearText = ref<HTMLInputElement | null>(null);
 
-export default defineComponent({
-  name: 'TransactionFilter',
-  data(): TransactionFilterData {
-    return {
-      month: new Date().getMonth(),
-      allMonths: monthNames,
-      year: new Date().getFullYear(),
-      editYear: false,
-      enteredYear: 0,
-    }
-  },
-  methods: {
-    startEditYear() {
-      this.enteredYear = this.year;
-      this.editYear = true;
-      this.$nextTick(() => {
-        this.$refs.yearText.focus();
-      })
-    },
-    finishEditYear(e: Event) {
-      console.log('Cancel')
-      e.preventDefault();
-      if (this.enteredYear <= new Date().getFullYear() && this.enteredYear > 1970) {
-        this.year = this.enteredYear
-      }
-      
-      this.editYear = false;
-    },
-    changeMonth(month: number) {
-      this.month = month;
-    },
-    monthActive(index: number) {
-      return this.month === index ? 'bg-primary' : 'monthbox-bg';
-    }
-  },
-  emits: ['filterChanged'],
-  watch: {
-    month() {
-      this.$emit('filterChanged', this.month, this.year);
-    },
-     year() {
-       this.$emit('filterChanged', this.month, this.year);
-     } 
+const selectedMonth = ref<number>(new Date().getMonth());
+const year = ref<number>(new Date().getFullYear());
+const editYear = ref<boolean>(false);
+const enteredYear = ref<number>(0);
+
+const allMonths = ref(monthNames);
+
+const { mobile } = useDisplay();
+
+const emits = defineEmits<{
+  (_e: 'filterChanged', _month: number, _year: number): true;
+}>();
+
+watch(selectedMonth, () => {
+  emits('filterChanged', selectedMonth.value, year.value);
+});
+watch(year, () => {
+  emits('filterChanged', selectedMonth.value, year.value);
+});
+
+const startEditYear = () => {
+  enteredYear.value = year.value;
+  editYear.value = true;
+  nextTick(() => {
+    yearText.value!!.focus();
+  });
+};
+
+const finishEditYear = (e: Event) => {
+  console.log('Cancel');
+  e.preventDefault();
+  if (
+    enteredYear.value <= new Date().getFullYear() &&
+    enteredYear.value > 1970
+  ) {
+    year.value = enteredYear.value;
   }
-})
+
+  editYear.value = false;
+};
+
+const changeMonth = (clickedMonth: number) => {
+  selectedMonth.value = clickedMonth;
+};
+
+const monthActive = (index: number) => {
+  return selectedMonth.value === index ? 'bg-primary' : 'monthbox-bg';
+};
 </script>
 
 <style scoped>
-  .month-box {
-    cursor: pointer;
-  }
-  .month-box:first-of-type {
-    border-radius: 15px 0 0 15px;
-  }
-  .month-box:last-of-type {
-    border-radius: 0 15px 15px 0;
-  }
-  
-  .monthbox-bg {
-    background-color: rgb(var(--v-theme-surface));
-  }
+.month-box {
+  cursor: pointer;
+}
+.month-box:first-of-type {
+  border-radius: 15px 0 0 15px;
+}
+.month-box:last-of-type {
+  border-radius: 0 15px 15px 0;
+}
+
+.monthbox-bg {
+  background-color: rgb(var(--v-theme-surface));
+}
 </style>
--- src/homebudget-frontend/src/utils/utils.ts
+++ src/homebudget-frontend/src/utils/utils.ts
@@ -2,23 +2,28 @@ export function getDateString(date: Date): string {
   return date.toISOString().split('T')[0];
 }
 
-export const monthNames = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
+interface MonthName {
+  index: number;
+  name: string;
+}
+
+export const monthNames: MonthName[] = [
+  { index: 0, name: 'January' },
+  { index: 1, name: 'February' },
+  { index: 2, name: 'March' },
+  { index: 3, name: 'April' },
+  { index: 4, name: 'May' },
+  { index: 5, name: 'June' },
+  { index: 6, name: 'July' },
+  { index: 7, name: 'August' },
+  { index: 8, name: 'September' },
+  { index: 9, name: 'October' },
+  { index: 10, name: 'November' },
+  { index: 11, name: 'December' },
 ];
 
 export function getMonthNameString(date: Date): string {
-  return monthNames[date.getMonth()];
+  return monthNames[date.getMonth()].name;
 }
 
 export function getErrorMessage(error: unknown) {
